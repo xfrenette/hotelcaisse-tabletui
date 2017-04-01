7ec6b7066b482ce8e379186fbf4b68103d3d55a5
Xavier Frenette <xavier@xavierfrenette.com>
2017-04-01 19:40:52 -0400

    Base setup for the Home screen

--- app/containers/screens/Home.js
+++ app/containers/screens/Home.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Link } from 'react-router-native';
+import { inject, observer } from 'mobx-react/native';
+import { STATES as REGISTER_STATES } from 'hotelcaisse-app/dist/business/Register';
+import HomeScreen from '../../components/screens/Home';
 
+@inject('business', 'router')
+@observer
 class Home extends Component {
+	onLinkPressed(key) {
+		let path = null;
+
+		switch (key) {
+			case 'open-register':
+				path = '/register/open';
+				break;
+			case 'close-register':
+				path = '/register/close';
+				break;
+			case 'manage-register':
+				path = '/register/manage';
+				break;
+			case 'new-order':
+				path = '/orders/new';
+				break;
+			case 'find-order':
+				path = '/orders/find';
+				break;
+		}
+
+		if (path) {
+			this.goToPath(path);
+		}
+	}
+
+	goToPath(path) {
+		if (this.props.router) {
+			this.props.router.push(path);
+		}
+	}
+
 	render() {
+		const registerState = this.props.business.deviceRegister
+			? this.props.business.deviceRegister.state
+			: REGISTER_STATES.CLOSED;
+
 		return (
-			<View style={{ backgroundColor: '#abcdef', flex: 1 }}>
-				<Link to="/register/open"><Text>Vers OpenRegister</Text></Link>
-			</View>
+			<HomeScreen
+				registerState={registerState}
+				onLinkPressed={(key) => { this.onLinkPressed(key); }}
+			/>
 		);
 	}
 }
