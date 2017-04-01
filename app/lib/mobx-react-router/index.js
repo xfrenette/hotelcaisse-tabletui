6ee8b86f126017653ff8288cc9ad7c710e7f0a9e
Xavier Frenette <xavier@xavierfrenette.com>
2017-04-01 19:38:18 -0400

    mobx-react-router/index.js : added a comment

--- app/lib/mobx-react-router/index.js
+++ app/lib/mobx-react-router/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 /**
  * Original mobx-react-router copied here and modified to fix some bugs
+ *
+ * @see  https://github.com/alisd23/mobx-react-router
  */
 export * from './src/store';
 export * from './src/sync';
