0f64818e0a32fcde466135597e71c585659fb9be
Xavier Frenette <xavier@xavierfrenette.com>
2017-04-28 14:48:28 -0400

    UILogger now uses the new Logger mixin

--- app/lib/UILogger.js
+++ app/lib/UILogger.js
@@ -1,14 +1,11 @@
-import Logger from 'hotelcaisse-app/dist/loggers/Logger';
+import { mixin as loggerMixin } from 'hotelcaisse-app/dist/loggers/Logger';
 import EventEmitter from 'events';
 
-class UILogger extends EventEmitter {
+class UILogger extends loggerMixin(EventEmitter) {
 	log(type, namespace, message, data) {
 		const now = new Date();
 		this.emit('log', now, type, namespace, message, data);
 	}
 }
-
-// False extending of Logger
-UILogger.prototype.getNamespace = Logger.prototype.getNamespace;
 
 export default UILogger;
