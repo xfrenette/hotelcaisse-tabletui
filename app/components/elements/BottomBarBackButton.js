1e5a88c5cca98e983f25b47df6b4f8f3413b9f40
Xavier Frenette <xavier@xavierfrenette.com>
2017-05-03 21:12:11 -0400

    BottomBarBackButton: small style fix

--- app/components/elements/BottomBarBackButton.js
+++ app/components/elements/BottomBarBackButton.js
@@ -19,6 +19,7 @@ const BottomBarBackButton = props => (
 		type="back"
 		hitSlop={props.hitSlop || { top: 20, right: 20, bottom: 20, left: 20 }}
 		layout={buttonLayouts.text}
+		touchEffect="opacity"
 		{...props}
 	/>
 );
