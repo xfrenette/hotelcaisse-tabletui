65327a9913ad8d8fda10a2a6ef2fc2bced2fe4f0
Xavier Frenette <xavier@xavierfrenette.com>
2017-05-03 21:11:56 -0400

    Sidebar: now has a scrollTop method

--- app/components/layout/Sidebar.js
+++ app/components/layout/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 import styleVars from '../../styles/variables';
@@ -13,14 +13,33 @@ const defaultProps = {
 	children: null,
 };
 
-const Sidebar = props => (
-	<ScrollView
-		style={[styles.scrollview, props.style]}
-		contentContainerStyle={styles.sidebar}
-	>
-		{ props.children }
-	</ScrollView>
-);
+class Sidebar extends Component {
+	/**
+	 * Internal references to Components
+	 *
+	 * @type {Object}
+	 */
+	nodeRefs = {};
+
+	/**
+	 * Scrolls the ScrollView to the top (no animation)
+	 */
+	scrollTop() {
+		this.nodeRefs.scrollView.scrollTo({ x: 0, y: 0, animated: false });
+	}
+
+	render() {
+		return (
+			<ScrollView
+				ref={(node) => { this.nodeRefs.scrollView = node; }}
+				style={[styles.scrollview, this.props.style]}
+				contentContainerStyle={styles.sidebar}
+			>
+				{ this.props.children }
+			</ScrollView>
+		);
+	}
+}
 
 Sidebar.propTypes = propTypes;
 Sidebar.defaultProps = defaultProps;
@@ -31,7 +50,7 @@ const styles = {
 		backgroundColor: styleVars.colors.darkGrey1,
 	},
 	sidebar: {
-		paddingHorizontal: styleVars.horizontalRhythm * 2,
+		paddingHorizontal: styleVars.horizontalRhythm,
 		paddingVertical: styleVars.verticalRhythm * 2,
 	},
 };
