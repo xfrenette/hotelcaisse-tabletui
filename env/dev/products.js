bb43c64e94c5961fbafdb350ae18796839bfacba
Xavier Frenette <xavier@xavierfrenette.com>
2017-05-28 17:28:59 -0400

    Dev env: added multiple products and a script to simplify the creation of dev products

---
+++ env/dev/products.js
@@ -0,0 +1,94 @@
+import Decimal from 'decimal.js';
+import Product from 'hotelcaisse-app/dist/business/Product';
+
+let uuid = 0;
+
+const productsData = [
+	{
+		name: 'Chambre privée',
+		description: 'Pour un ou deux adultes seulement',
+		variants: [70, 62.5],
+	},
+	{
+		name: 'Dortoir',
+		variants: [30, 25],
+	},
+	{
+		name: 'Chambre familiale',
+		description: 'Un ou deux adultes + un ou deux enfants (0-17 ans)',
+		variants: [96, 85],
+	},
+	{
+		name: 'Adulte',
+		variants: [30, 25],
+	},
+	{
+		name: 'Enfant (7-17 ans)',
+		variants: [15, 12.5],
+	},
+	{
+		name: 'Enfant (0-6 ans)',
+		price: 0,
+	},
+	{
+		name: 'Souper',
+		variants: [12.5, 11],
+	},
+	{
+		name: 'Carte de membre HI',
+		price: 28.75,
+	},
+	{
+		name: 'Internet',
+		price: 1,
+	},
+	{
+		name: 'Lavage',
+		price: 5,
+	},
+];
+
+function getUUID() {
+	const res = uuid;
+	uuid += 1;
+	return `prod_${res}`;
+}
+
+function setProductPrice(product, rawPrice) {
+	const price = new Decimal(rawPrice);
+	const netPrice = price.div(1.14975);
+	const tps = netPrice.mul(0.05).toDecimalPlaces(2);
+	const tvq = netPrice.mul(0.09975).toDecimalPlaces(2);
+
+	product.price = price.sub(tps).sub(tvq);
+	product.addTax('TPS', tps);
+	product.addTax('TVQ', tvq);
+}
+
+const variantLabels = ['Non-membre', 'Membre'];
+
+const products = productsData.map((productData) => {
+	const product = new Product();
+	product.uuid = getUUID();
+	product.name = productData.name;
+	product.description = productData.description;
+
+	if (productData.variants) {
+		productData.variants.forEach((variant, index) => {
+			const variantProduct = new Product();
+			variantProduct.uuid = getUUID();
+			variantProduct.name = variantLabels[index];
+			setProductPrice(variantProduct, variant);
+
+			product.addVariant(variantProduct);
+		});
+	}
+
+	if (productData.price) {
+		setProductPrice(product, productData.price);
+	}
+
+	return product;
+});
+
+export default products;
--- env/dev/storedBusiness.js
+++ env/dev/storedBusiness.js
@@ -7,6 +7,7 @@ import Register from 'hotelcaisse-app/dist/business/Register';
 import Room from 'hotelcaisse-app/dist/business/Room';
 import TransactionMode from 'hotelcaisse-app/dist/business/TransactionMode';
 import { TextField, EmailField, SelectField, PhoneField, NumberField } from 'hotelcaisse-app/dist/fields';
+import products from './products';
 
 /**
  * Returns a Business instance that is the "locally saved" Business instance when the app starts.
@@ -29,53 +30,17 @@ register.addCashMovement(cashMovement2);
 const rootCategory = new ProductCategory();
 rootCategory.uuid = '__root__';
 
-const product1 = new Product();
-product1.uuid = 'p1';
-product1.name = 'Chambre privée';
-product1.description = 'Pour un ou deux adultes seulement';
-
-const product11 = new Product();
-product11.uuid = 'p11';
-product11.name = 'Membre';
-product11.price = new Decimal(60);
-product11.addTax('TPS', new Decimal(3));
-product11.addTax('TVQ', new Decimal(5.45));
-
-const product12 = new Product();
-product12.uuid = 'p12';
-product12.name = 'Non-membre';
-product12.price = new Decimal(70);
-product12.addTax('TPS', new Decimal(4));
-product12.addTax('TVQ', new Decimal(6.45));
-
-product1.addVariant(product11);
-product1.addVariant(product12);
-
-const product2 = new Product();
-product2.uuid = 'p2';
-product2.name = 'Dortoir';
-product2.price = new Decimal(20);
-product2.addTax('TPS', new Decimal(2));
-product2.addTax('TVQ', new Decimal(3.68));
-
-const product3 = new Product();
-product3.uuid = 'p3';
-product3.name = 'Adulte';
-product3.price = new Decimal(20);
-product3.addTax('TPS', new Decimal(2));
-product3.addTax('TVQ', new Decimal(3.68));
-
-rootCategory.products.push(product1);
-rootCategory.products.push(product2);
+rootCategory.products = [products[0], products[1], products[2]];
 
 const subCategory1 = new ProductCategory();
 subCategory1.uuid = 'sc1';
 subCategory1.name = 'Personne additionnelle';
-subCategory1.products.push(product3);
+subCategory1.products = [products[3], products[4], products[5]];
 
 const subCategory2 = new ProductCategory();
 subCategory2.uuid = 'sc2';
 subCategory2.name = 'Divers';
+subCategory2.products = [products[6], products[7], products[8], products[9]];
 
 rootCategory.categories.push(subCategory1);
 rootCategory.categories.push(subCategory2);
@@ -84,7 +49,7 @@ const business = new Business();
 business.uuid = 'business-uuid';
 business.deviceRegister = register;
 business.rootProductCategory = rootCategory;
-business.products = [product1, product2, product3, product11, product12];
+business.products = products;
 
 const nameField = new TextField();
 nameField.uuid = 'name-field';
