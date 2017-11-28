976dd82fc2a837d4944ab4341606909260310a54
Xavier Frenette <xavier@xavierfrenette.com>
2017-11-28 17:31:17 -0500

    Log: now truncates long messages (with an alert to show the rest) and small fix to the display of date times

--- app/components/screens/dev/Log.js
+++ app/components/screens/dev/Log.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, Text, View, } from 'react-native';
+import { ScrollView, Text, View, TouchableHighlight, Alert } from 'react-native';
 import { PropTypes as MobxPropTypes } from 'mobx-react';
 import { observer } from 'mobx-react/native';
 import { MainContent, Screen, TopBar } from '../../layout';
@@ -18,6 +18,12 @@ const defaultProps = {
 	onPressHome: null,
 };
 
+function pad(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 @observer
 class Log extends Component {
 	/**
@@ -32,6 +38,10 @@ class Log extends Component {
 		}
 
 		return path;
+	}
+
+	onShowFullData(entry, formattedText) {
+		Alert.alert(entry.message, formattedText);
 	}
 
 	/**
@@ -61,12 +71,39 @@ class Log extends Component {
 		}
 
 		if (entry.data) {
-			const dataText = typeof entry.data === 'string' ? entry.data : JSON.stringify(entry.data);
-			data = <View style={styles.entryData}><Text>{ dataText }</Text></View>;
+			const previewLength = 100;
+			let dataString = typeof entry.data === 'string' ? entry.data : JSON.stringify(entry.data);
+			let moreButton = null;
+
+			if (dataString.length > previewLength) {
+				const fullText = dataString;
+				dataString = `${dataString.substring(0, previewLength)} ... `;
+				moreButton = (
+					<TouchableHighlight
+						onPress={() => { this.onShowFullData(entry, fullText) }}
+					>
+						<Text style={styles.viewFull}>View full</Text>
+					</TouchableHighlight>
+				);
+			}
+
+			data = (
+				<View style={styles.entryData}>
+					<Text>{ dataString }</Text>
+					{ moreButton }
+				</View>
+			);
 		}
 
 		const dateTime = entry.date;
-		const dateTimeText = `${dateTime.getFullYear()}-${dateTime.getMonth() + 1}-${dateTime.getDate()} ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}.${dateTime.getMilliseconds()}`;
+		const y = dateTime.getFullYear();
+		const M = pad(dateTime.getMonth() + 1, 2);
+		const d = pad(dateTime.getDate(), 2);
+		const h = pad(dateTime.getHours(), 2);
+		const m = pad(dateTime.getMinutes(), 2);
+		const s = pad(dateTime.getSeconds(), 2);
+		const ms = pad(dateTime.getMilliseconds(), 3);
+		const dateTimeText = `${y}-${M}-${d} ${h}:${m}:${s}.${ms}`;
 
 		return (
 			<View key={index}>
@@ -125,6 +162,7 @@ const styles = {
 		right: 0,
 		bottom: 0,
 	},
+
 	entries: {
 		padding,
 	},
@@ -134,10 +172,14 @@ const styles = {
 		color: '#999',
 	},
 
-	entryDate: {
+	entryData: {
+		marginLeft: 30,
+		justifyContent: 'flex-start',
+		flexDirection: 'row',
 	},
 
-	entryData: {
+	viewFull: {
+		color: 'green',
 	},
 
 	actions: {
